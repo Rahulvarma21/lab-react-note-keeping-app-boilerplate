337b3b3a733b55569baeeb36f402d771aead9f71
Rahul <rahul@Rahuls-MacBook-Air.local>
2024-02-05 22:54:30 +0530

    completed

---
+++ pronote/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Editor from '../src/Components/Editor';
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Editor />
+      {/* Other components or content */}
+    </div>
+  );
+}
+
+export default App;
